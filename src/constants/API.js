7289f5079f79e6152d53f637bd9954921195a892
virugamacoder <krunal.virugama@tinyscript.in>
2024-08-23 20:22:49 +0530

    base url change

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://evitalrx-backend.onrender.com";
 export const signUpSendOtpAPI = `${baseUrl}/signupsendotp`;
 export const signUpVerifyOtpAPI = `${baseUrl}/signupverifyotp`;
 export const loginAPI = `${baseUrl}/login`;
